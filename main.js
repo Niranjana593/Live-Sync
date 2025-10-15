270fb3abde309badfb295cfe3961d6239978609a
Rashmi <Niranjan1@gmail.com>
2025-10-15 19:39:02 +0530

    Defining the preload.js file

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const { app, BrowserWindow } = require('electron')
-
+const path = require('path')
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadURL('http://localhost:5173/')
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow()// this for the window/linux os. 
+  app.on('activate', () => {// This is for mac os.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
