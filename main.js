d3418d0d5d3bbda15c080525f9d7b18b008f1fdd
Rashmi <Niranjan1@gmail.com>
2025-10-24 19:41:49 +0530

    extra lines of boiler-plate code removed

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-const { app, BrowserWindow ,ipcMain} = require('electron')
+const { app, BrowserWindow ,ipcMain,dialog} = require('electron')
 const path = require('path')
 ipcMain.handle('data-change',async(event,message)=>{
   console.log(message);
   return 'Recieve your message'
 })
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration:false
     }
   })
 
@@ -22,6 +25,31 @@ app.whenReady().then(() => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
+//Source file selector
+ipcMain.handle('select-source', async (event) => {
+  const { canceled, filePaths } = await dialog.showOpenDialog({
+    properties: ['openFile']
+  });
+  
+  if (canceled) {
+    return null;  // user canceled, return null
+  }
+
+  return filePaths[0];  // return first selected path
+});
+//Destination file selector
+ipcMain.handle('select-destination', async (event) => {
+  const { canceled, filePaths } = await dialog.showOpenDialog({
+    properties: ['openFile']
+  });
+  
+  if (canceled) {
+    return null;  // user canceled, return null
+  }
+
+  return filePaths[0];  // return first selected path
+});
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
